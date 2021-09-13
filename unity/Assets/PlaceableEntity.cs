29fe7888e87f864ed79b4f7fab1fcbc2330599e1
Bernat <SZE19728@spengergasse.at>
2021-09-13 11:30:31 +0200

    13. September Checkout

--- unity/Assets/PlaceableEntity.cs
+++ unity/Assets/PlaceableEntity.cs
@@ -6,7 +6,9 @@ using UnityEngine.AI;
 public class PlaceableEntity : MonoBehaviour
 {
     public Vector3 _destination;
+    public float radius = 10;
     NavMeshAgent agent;
+    public Collider dest = null;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +18,19 @@ public class PlaceableEntity : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        agent.destination = new Vector3(11, 0, 3);
+        //agent.destination = new Vector3(11, 0, 3);
+        Collider[] inRange = Physics.OverlapSphere(transform.position, radius);
+        
+        float dist = float.MaxValue;
+        foreach (Collider col in inRange)
+        {
+            if ((col.transform.position - transform.position).magnitude < dist)
+            {
+                dest = col;
+            }
+        }
+        if (dest != null)
+            _destination = dest.transform.position;
+        agent.destination = _destination;
     }
 }
