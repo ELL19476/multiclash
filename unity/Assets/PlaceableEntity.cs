95725a69493fe27712761eb89b42c6073b4773f1
Bernat <SZE19728@spengergasse.at>
2021-09-13 09:45:35 +0200

    navmesh setup

--- unity/Assets/PlaceableEntity.cs
+++ unity/Assets/PlaceableEntity.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlaceableEntity : MonoBehaviour
 {
+    NavMeshAgent agent;
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime);
+        //transform.Translate(Vector3.forward * Time.deltaTime);
+        agent.destination = new Vector3(11, 0, 3);
     }
 }
